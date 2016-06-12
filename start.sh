d81b2a1fac1742deb66b578e030c13f0db7409bf
Yevgen Fedorenko <funarg@live.com>
2016-06-12 23:43:29 +0300

    Increase logging level

--- start.sh
+++ start.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-set -e
+#set -e
 
 TUNNEL_IP=${IODINE_TUNNEL_IP:-"10.10.10.1"}
 
 #sysctl -w net.ipv4.ip_forward=1
+iptables -t nat -D POSTROUTING -s 10.10.10.0/24 -o br0 -j MASQUERADE
+iptables -t nat -D POSTROUTING -s 10.10.10.0/24 -o br0 -j MASQUERADE
 iptables -t nat -A POSTROUTING -s 10.10.10.0/24 -o br0 -j MASQUERADE
 
 iptables -S
 iptables -S -t nat
 
-iodined -c -m 1280 -D -f $TUNNEL_IP $IODINE_HOST -P $IODINE_PASSWORD -n $EXTERNAL_IP
+iodined -c -m 1280 -DD -f $TUNNEL_IP $IODINE_HOST -P $IODINE_PASSWORD -n $EXTERNAL_IP
